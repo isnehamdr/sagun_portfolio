01117961555579e4c96b50975190f9f3dff17e5e
Isneha Manandhar <isnehamanandhar88@gmail.com>
2026-04-06 14:02:26 +0545

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+   <>
+   <div className="bg-[#fefef3] text-[#c7482c] uppercase text-[12rem] font-bold text-center tracking-[3px] " style={{fontFamily:'Playfair Display'}}>
+    <h1>Shagun</h1>
+    <h2>Manandhar</h2>
+
+   </div>
+   
+   
+   </>
+  )
+}
+
+export default Hero
